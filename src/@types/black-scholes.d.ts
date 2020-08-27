24c77294e6ca4ccf390d1e365c4ec7d064e109c8
Will Coughlin <willcoughlin@gmail.com>
2020-08-27 12:41:11 -0500

    Define return type for blackScholes function

--- src/@types/black-scholes.d.ts
+++ src/@types/black-scholes.d.ts
@@ -1,3 +1,3 @@
 declare module 'black-scholes' {
-  export function blackScholes(s: number, k: number, t: number, v: number, r: number, callPut: 'call' | 'put');
+  export function blackScholes(s: number, k: number, t: number, v: number, r: number, callPut: 'call' | 'put'): number;
 }

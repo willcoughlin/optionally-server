1cf9c60e96a57d69eb529059b2dbbac4449958f6
Will Coughlin <willcoughlin@gmail.com>
2020-08-27 12:42:00 -0500

    Create calculator module for profit/loss

---
+++ src/util/return-calculator.ts
@@ -0,0 +1,55 @@
+import { CalculatorInput, StrategyType } from "../graphql/types";
+import { GQLSafeNumber } from "./types";
+
+/**
+ * Calculates entry cost (or credit) of a given input.
+ * @param input input of type CalculatorInput
+ * @returns entry cost or credit as a number
+ */
+export function calclateEntryCost(input: CalculatorInput) {
+  switch (input.strategy) {
+    case StrategyType.Call:
+      if (input.longCall)
+        return input.longCall.currentPrice;
+      if (input.shortCall)
+        return -input.shortCall.currentPrice;
+      throw new Error('longCall or shortCall must be defined for StrategyType.Call');
+      
+    case StrategyType.Put:
+      if (input.longPut)
+        return input.longPut.currentPrice;
+      if (input.shortPut)
+        return -input.shortPut.currentPrice;
+      throw new Error('longPut or shortPut must be defined for StrategyType.Put');
+    
+    case StrategyType.StraddleStrangle:
+      if (input.longCall && input.longPut)
+        return input.longCall.currentPrice + input.longPut.currentPrice;
+      if (input.shortCall && input.shortPut)
+        return -input.shortCall.currentPrice - input.shortPut.currentPrice;
+      throw new Error('Both longCall and longPut or shortCall and shortPut must be defined for StrategyType.StraddleStrangle');
+
+    default:
+      throw new Error('Not implemented');
+  }
+}
+
+/**
+ * Calculates maximum risk and maximum return
+ * @param input input of type CalculatorInput
+ * @returns a tuple of [maxRisk, maxReward]
+ */
+export function calculateMaxRiskAndReturn(input: CalculatorInput): [GQLSafeNumber, GQLSafeNumber] {
+  switch (input.strategy) {
+    case StrategyType.Call:
+    case StrategyType.Put:
+    case StrategyType.StraddleStrangle:
+      const entryCost = calclateEntryCost(input);
+      if (entryCost < 0)
+        return [null, entryCost];
+      return [entryCost, null];
+    
+    default:
+      throw new Error('Not implemented');
+  }
+}

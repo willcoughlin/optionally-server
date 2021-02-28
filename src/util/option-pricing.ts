047b1b6df063588be7b7a3b2647e39091b79f343
Will Coughlin <willcoughlin@gmail.com>
2021-02-27 21:30:17 -0600

    Update option pricing function to use new GQL type

--- src/util/option-pricing.ts
+++ src/util/option-pricing.ts
@@ -1,12 +1,13 @@
 import { blackScholes } from 'black-scholes';
-import moment, { Moment } from "moment";
-import { Option, OptionType } from '../graphql/types';
+import moment, { Moment } from 'moment';
+import { OptionType } from '../graphql/types';
+import { OptionInput } from './../graphql/types';
 
 export function calculateApproximateRiskFreeInterestRate(tBillRate: number, inflationRate: number) {
   return tBillRate + inflationRate;
 }
 
-export function calculateOptionPriceForDates(option: Option, riskFreeInterestRate: number, dates: Moment[]) {
+export function calculateOptionPriceForDates(option: OptionInput, riskFreeInterestRate: number, dates: Moment[]) {
   if (dates.length == 0) return [];
 
   const expiry = moment(option.expiry);

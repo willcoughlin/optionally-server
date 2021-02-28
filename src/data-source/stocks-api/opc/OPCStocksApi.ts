b7a4efe65556d84ac667daba1e7bc11092209904
Will Coughlin <willcoughlin@gmail.com>
2021-02-28 12:28:27 -0600

    Insert placeholder IV for OPC stock api

--- src/data-source/stocks-api/opc/OPCStocksApi.ts
+++ src/data-source/stocks-api/opc/OPCStocksApi.ts
@@ -96,7 +96,7 @@ export default class OPCStocksApi extends RESTDataSource implements IStocksApi {
         last: parseFloat(contract.l),
         strike: parseFloat(strike),
         type: type,
-        impliedVolatility: parseFloat(contract.v)
+        impliedVolatility: 10 // parseFloat(contract.v) TODO: this was actually volume. need to get real IV
       };
     }
   }
